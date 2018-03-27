788472dd47cff3a4e4f53ca646c0b23c7077acd0
JamesKirkAndSpock <crispuskamau77@gmail.com>
2018-03-27 20:19:56 +0300

    chore(user-determination): change if statement determination
    Change the if statement determination to determine a Circle CI user
    
    [ Fixes #156068297 ]

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -21,23 +21,53 @@ prepare_deployment_script() {
     echo ${SERVICE_ACCOUNT} > ~/Hirola-Deployment-Script/account-folder/account.json
 }
 
+check_branch(){
+    echo "$CIRCLE_USERNAME"
+    if [[ "$CIRCLE_USERNAME" == "JamesKirkAndSpock" && "$CIRCLE_BRANCH" != 'develop' && "$CIRCLE_BRANCH" != 'master' ]]; then
+        HOST=${DEVOPS_HOST}
+        ENVIRONMENT=${DEVOPS_ENVIRONMENT}
+        IP_ADDRESS=${DEVOPS_IP_ADDRESS}
+    fi
+
+    if [ "$CIRCLE_BRANCH" == 'develop' ]; then
+        HOST=${DEVELOP_HOST}
+        ENVIRONMENT=${DEVELOP_ENVIRONMENT}
+        IP_ADDRESS=${DEVELOP_IP_ADDRESS}
+    fi
+
+    if [ "$CIRCLE_BRANCH" == 'master' ]; then
+        HOST=${MASTER_HOST}
+        ENVIRONMENT=${MASTER_ENVIRONMENT}
+        IP_ADDRESS=${MASTER_IP_ADDRESS}
+    fi
+}
+
 initialise_terraform() {
     cd ~/Hirola-Deployment-Script
     terraform init -backend-config=bucket="${GCP_BUCKET}" -backend-config=prefix="/hirola-terraform/${ENVIRONMENT}/terraform.tfstate"
 }
 
 destroy_previous_infrastructure(){
-    terraform destroy -lock=false -auto-approve -var=project="${PROJECT_ID}" -var=ip-address="${IP_ADDRESS}" -var=env="${ENVIRONMENT}" -var=branch="${CIRCLE_BRANCH}" -var=host="${HOST}"
+    if [[ "$CIRCLE_USERNAME" == "JamesKirkAndSpock" && "$CIRCLE_BRANCH" != 'develop' && "$CIRCLE_BRANCH" != 'master' ]]; then
+        terraform destroy -auto-approve -var=project="${PROJECT_ID}" -var=ip-address="${IP_ADDRESS}" -var=env="${ENVIRONMENT}" -var=branch="${CIRCLE_BRANCH}" -var=host="${HOST}" -var=region="${DEVOPS_REGION}" -var=zone="${DEVOPS_ZONE}"
+    else
+        terraform destroy -auto-approve -var=project="${PROJECT_ID}" -var=ip-address="${IP_ADDRESS}" -var=env="${ENVIRONMENT}" -var=branch="${CIRCLE_BRANCH}" -var=host="${HOST}"
+    fi
 
 }
 
 build_current_infrastructure() {
-    terraform apply -lock=false -auto-approve -var=project="${PROJECT_ID}" -var=ip-address="${IP_ADDRESS}" -var=env="${ENVIRONMENT}" -var=branch="${CIRCLE_BRANCH}" -var=host="${HOST}"
+    if [[ "$CIRCLE_USERNAME" == "JamesKirkAndSpock" && "$CIRCLE_BRANCH" != 'develop' && "$CIRCLE_BRANCH" != 'master' ]]; then
+        terraform apply -auto-approve -var=project="${PROJECT_ID}" -var=ip-address="${IP_ADDRESS}" -var=env="${ENVIRONMENT}" -var=branch="${CIRCLE_BRANCH}" -var=host="${HOST}" -var=region="${DEVOPS_REGION}" -var=zone="${DEVOPS_ZONE}"
+    else
+        terraform apply -auto-approve -var=project="${PROJECT_ID}" -var=ip-address="${IP_ADDRESS}" -var=env="${ENVIRONMENT}" -var=branch="${CIRCLE_BRANCH}" -var=host="${HOST}"
+    fi
 }
 
 main() {
     download_terraform
     prepare_deployment_script
+    check_branch
     initialise_terraform
     destroy_previous_infrastructure
     build_current_infrastructure
