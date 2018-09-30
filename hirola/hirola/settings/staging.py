cd4f26adc32c6c72b743363bac99c12e8212a692
JamesKirkAndSpock <crispuskamau77@gmail.com>
2018-09-30 08:45:45 +0300

    feat(http-to-https): use https encryption
    - server the application with gunicorn.
    - collect static files for nginx.
    - server the application with Debug being false
    
    [  Fixes #156068329 ]

--- hirola/hirola/settings/staging.py
+++ hirola/hirola/settings/staging.py
@@ -1,6 +1,7 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')
 MEDIA_URL = os.environ.get('GS_BUCKET_URL')
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+STATIC_ROOT = '/var/www/html/static/'
