5b3f28cb958880a598c217bd470059ab9cbd5dc5
JamesKirkAndSpock <crispuskamau77@gmail.com>
2018-10-29 21:23:53 +0300

    test3

--- hirola/hirola/settings/staging.py
+++ hirola/hirola/settings/staging.py
@@ -5,5 +5,5 @@ GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')
 MEDIA_URL = os.environ.get('GS_BUCKET_URL')
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 STATIC_ROOT = '/var/www/html/static/'
-SESSION_COOKIE_AGE=int(os.environ.get('SESSION_COOKIE_AGE'), base=0)
-SESSION_COOKIE_AGE_REMEMBER=int(os.environ.get('SESSION_COOKIE_AGE_KNOWN_DEVICE'))
+# SESSION_COOKIE_AGE=int(os.environ.get('SESSION_COOKIE_AGE'), base=0)
+# SESSION_COOKIE_AGE_REMEMBER=int(os.environ.get('SESSION_COOKIE_AGE_KNOWN_DEVICE'))
