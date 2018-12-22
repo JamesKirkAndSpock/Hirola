4ce6cf3a9c1175897b1e13f4dfed918d87ca3d61
JamesKirkAndSpock <crispuskamau77@gmail.com>
2018-12-22 14:19:50 +0300

    chore(test): organize tests
    - categorize tests properly
    
    [ Fixes #162707672]

---
+++ hirola/front/tests/dashboard/selenium_dashboard.py
@@ -0,0 +1,57 @@
+from front.tests.base_selenium import *
+
+
+class MyOrders(BaseSeleniumTestCase):
+    '''
+    Test that the My Orders Page functions as expected
+    '''
+
+    def setUp(self):
+        super(MyOrders, self).setUp()
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.create_country_code()
+        self.create_user()
+        self.create_phone_category()
+        self.create_phone_memory_size()
+        self.create_currency()
+        self.create_item_icon()
+        self.create_phone_list()
+        self.create_order_status()
+        self.create_payment_method()
+        self.create_order()
+        self.create_shipping_address()
+
+
+    def login_user(self):
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/login'))
+        email_input = driver.find_element_by_name("email")
+        email_input.send_keys("timonpumba@gmail.com")
+        password_input = driver.find_element_by_name("password")
+        password_input.send_keys("secret")
+        submit_button = driver.find_element_by_name("action")
+        submit_button.click()
+        return driver
+
+
+
+    def test_order_details_link(self):
+        '''
+        Test that when a user moves to the order_detail and clicks on the MY ORDERS link:
+            - That the link opens up the orders that the user has
+        '''
+        driver = self.login_user()
+        driver.get('%s%s' % (self.live_server_url, '/dashboard'))
+        order_link = driver.find_element_by_link_text("MY ORDERS")
+        order_link.click()
+        order_1 = driver.find_element_by_id("details-1")
+        self.assertEqual(order_1.get_attribute('style'), '')
+        order_details_link = driver.find_element_by_id("order-details-button")
+        order_details_link.click()
+        self.assertEqual(order_1.get_attribute('style'), "display: block;")
+        order_details_link.click()
+        self.assertEqual(order_1.get_attribute('style'), "display: none;")
+
+    def tearDown(self):
+        self.driver.close()
---
+++ hirola/front/tests/profile/selenium_profile.py
@@ -0,0 +1,98 @@
+from front.tests.base_selenium import *
+
+
+class PhoneProfileLink(BaseSeleniumTestCase):
+    '''
+    Test that buttons, and links on the profile page will redirect to the
+    page intended
+    '''
+
+    def setUp(self):
+        super(PhoneProfileLink, self).setUp()
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
+        self.create_country_code()
+        self.create_phone_category()
+        self.create_phone_memory_size()
+        self.create_currency()
+        self.create_item_icon()
+        self.create_phone_list()
+        
+    def test_teke_link(self):
+        '''
+        Test that the teke link redirects to the landing page
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+        driver.find_element_by_id("profile-main-page").click()
+        self.assertEqual(driver.current_url, '%s%s' % (self.live_server_url, '/'))
+
+    def test_iphone_link(self):
+        '''
+        Test that the iphone link redirects to the phone category page for iphones
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+        driver.find_element_by_link_text("iPhone").click()
+        self.assertEqual(driver.current_url, '%s%s' % (self.live_server_url, '/phone_category/{}/'.format(self.iphone.pk)))
+
+    def test_phone_link(self):
+        '''
+        Test that the phone link redirects to the phone page for the specified
+        phone.
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+        self.assertEqual(driver.current_url, '%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+
+    def test_faq_link(self):
+        '''
+        Test that the FAQ link redirects to the help page
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+        driver.find_element_by_link_text("FAQs").click()
+        self.assertEqual(driver.current_url, '%s%s' % (self.live_server_url, '/help'))
+
+    def test_dashboard_link(self):
+        '''
+        Test that the dashboard link redirects to the dashboard page for a
+        user
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+        driver.find_element_by_link_text("My Teke").click()
+        self.assertEqual(driver.current_url,
+                         '%s%s' % (self.live_server_url, '/login?next=/dashboard'))
+
+    def test_navbar_teke_link(self):
+        '''
+        Test that the navbar teke link redirects to the landing page
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+        driver.find_element_by_id("base-main-page").click()
+        self.assertEqual(driver.current_url, '%s%s' % (self.live_server_url, '/'))
+
+    def test_cart_link(self):
+        '''
+        Test that the cart link on the profile page redirects to the checkout
+        page
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/{}/'.format(self.iphone.pk)))
+        element = driver.find_element_by_id("add-to-cart-button")
+        driver.find_element_by_id("add-to-cart-button").click()
+        self.assertEqual(driver.current_url, '%s%s' % (self.live_server_url, '/checkout'))
+
+    def test_missing_profile(self):
+        '''
+        Test that when you visit a profile that does not exist:
+        - That the user gets a 404 error message that the product does not exist
+        '''
+        driver = self.driver
+        driver.get('%s%s' % (self.live_server_url, '/profile/2123764872138/'))
+        self.assertEqual(driver.current_url, '%s%s' % (self.live_server_url, '/error'))
+
+    def tearDown(self):
+        self.driver.close()
