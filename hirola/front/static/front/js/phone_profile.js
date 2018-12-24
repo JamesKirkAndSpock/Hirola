9e092fb456e47627079caa7f8d590248ab62139d
JamesKirkAndSpock <crispuskamau77@gmail.com>
2018-12-24 22:27:24 +0300

    fix(js): fix phone profile js
    - fix the phone profile js
    
    [ Fixes #162663275 ]

--- hirola/front/static/front/js/phone_profile.js
+++ hirola/front/static/front/js/phone_profile.js
@@ -1,22 +1,4 @@
-// $('select#color').on('change', function () {
-//     var select = $('#quantityRow .dropdown-content');
-//     select.empty();
-//     var pk = this.value;
-//     Object.keys(quantities).forEach(function (key) {
-//         if(pk == quantities[key].color){
-//         var quantity = quantities[key].quantity;
-//             for(var i=1; i<=quantity; i++){
-//                 var li = $('<li></li>');
-//                 li.attr('value', i);
-//                 li.html(i);
-//                 select.append(li);
-//             }
-//         }
-//     });
-// });
-
 $('.materialSelect').on('change', function () {
-    // $('#quantity).not(:first)').empty();
     $('#quantityRow option').not(':first').remove();
     var pk = this.value;
     Object.keys(quantities).forEach(function (key) {
