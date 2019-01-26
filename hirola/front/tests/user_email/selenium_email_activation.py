1327495f46ccefd44c5ea5ab16e9bbfe9358d7cc
Peter <pndungu54@gmail.com>
2019-01-27 01:44:55 +0300

    Tidy up tests

--- hirola/front/tests/user_email/selenium_email_activation.py
+++ hirola/front/tests/user_email/selenium_email_activation.py
@@ -1,5 +1,4 @@
 from front.tests.base_selenium import *
-from selenium.webdriver.chrome.options import Options
 import time
 
 
@@ -11,9 +10,6 @@ class EmailSentPageLinks(BaseSeleniumTestCase):
 
     def setUp(self):
         super(EmailSentPageLinks, self).setUp()
-        # chrome_options = Options()
-        # chrome_options.add_argument("--window-size=1920,1080")
-        # self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver = webdriver.Chrome()
         self.create_country_code()
         self.driver.implicitly_wait(30)
@@ -41,15 +37,15 @@ class EmailSentPageLinks(BaseSeleniumTestCase):
     def test_resend_now_link(self):
         """Test resend now link redirects to login page after resending email."""
         self.driver.get('%s%s' % (self.live_server_url, '/signup'))
-        self.driver.find_element_by_name('first_name').send_keys('Peter')
-        self.driver.find_element_by_name('last_name').send_keys('Ndungu')
+        self.driver.find_element_by_name('first_name').send_keys('Van')
+        self.driver.find_element_by_name('last_name').send_keys('Bronckhorst')
         self.driver.find_element_by_class_name('select-wrapper').click()
         self.driver.find_element_by_xpath("//span[text()='+254 Kenya']").click()
-        self.driver.find_element_by_name('phone_number').send_keys('712705422')
+        self.driver.find_element_by_name('phone_number').send_keys('722000000')
         self.driver.find_element_by_name(
-            'email').send_keys('ndunguwanyinge@outlook.com')
-        self.driver.find_element_by_name('password1').send_keys('mrndungu2018')
-        self.driver.find_element_by_name('password2').send_keys('mrndungu2018')
+            'email').send_keys('van@outlook.com')
+        self.driver.find_element_by_name('password1').send_keys('mrvan2018')
+        self.driver.find_element_by_name('password2').send_keys('mrvan2018')
         self.driver.find_element_by_tag_name('button').click()
         self.assertEqual(self.driver.current_url, '%s%s' %
                          (self.live_server_url, '/signup'))
@@ -60,29 +56,30 @@ class EmailSentPageLinks(BaseSeleniumTestCase):
     def test_change_email(self):
         """Test change email."""
         self.driver.get('%s%s' % (self.live_server_url, '/signup'))
-        self.driver.find_element_by_name('first_name').send_keys('Peter')
-        self.driver.find_element_by_name('last_name').send_keys('Ndungu')
+        self.driver.find_element_by_name('first_name').send_keys('Van')
+        self.driver.find_element_by_name('last_name').send_keys('Bronckhorst')
         self.driver.find_element_by_class_name('select-wrapper').click()
         self.driver.find_element_by_xpath("//span[text()='+254 Kenya']").click()
-        self.driver.find_element_by_name('phone_number').send_keys('712705422')
+        self.driver.find_element_by_name('phone_number').send_keys('722000000')
         self.driver.find_element_by_name(
-            'email').send_keys('ndunguwanyinge@outlook.com')
-        self.driver.find_element_by_name('password1').send_keys('mrndungu2018')
-        self.driver.find_element_by_name('password2').send_keys('mrndungu2018')
+            'email').send_keys('van@outlook.com')
+        self.driver.find_element_by_name('password1').send_keys('mrvan2018')
+        self.driver.find_element_by_name('password2').send_keys('mrvan2018')
         self.driver.find_element_by_tag_name('button').click()
         self.assertEqual(self.driver.current_url, '%s%s' %
                          (self.live_server_url, '/signup'))
         self.driver.find_element_by_link_text('change now').click()
         self.assertEqual(self.driver.current_url, '%s%s' %
-                         (self.live_server_url, '/change_activation_email/ndunguwanyinge@outlook.com/'))
+                         (self.live_server_url, '/change_activation_email/van@outlook.com/'))
         self.driver.find_element_by_name(
-            'email').send_keys('pndungu54@gmail.com')
+            'email').send_keys('bronck@gmail.com')
         self.driver.find_element_by_tag_name('button').click()
         self.assertEqual(self.driver.current_url, '%s%s' %
-                         (self.live_server_url, '/send_link_to_new_address/ndunguwanyinge@outlook.com/'))
+                         (self.live_server_url, '/send_link_to_new_address/van@outlook.com/'))
     def tearDown(self):
         self.driver.stop_client()
         self.driver.close()
+
 
 class InactiveUserRedirect(BaseSeleniumTestCase):
 
