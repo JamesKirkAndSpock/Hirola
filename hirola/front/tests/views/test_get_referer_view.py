057d3c3cf19330c2deeb6dcd46ad1d85ae2a61b8
JamesKirkAndSpock <33254685+JamesKirkAndSpock@users.noreply.github.com>
2019-02-06 11:46:36 +0300

    feat(logout): logout a user out of the application
    - add a logout view that logs out the request of the user.
    - add functionality of redirecting a user back to the page he was on.
    - ensure a logout keeps the user on the same page
    
    [ Fixes #163739174]

---
+++ hirola/front/tests/views/test_get_referer_view.py
@@ -0,0 +1,33 @@
+""" A module that tests the functionality for the get_referer_view method """
+from front.base_test import BaseTestCase
+from front.views import get_referer_view
+from django.test import RequestFactory
+
+
+class GetReferViewTest(BaseTestCase):
+
+    def setUp(self):
+        super(GetReferViewTest, self).setUp()
+
+    def test_method_returns_home_page_without_referer(self):
+        """
+        Test that when you call the get_refer_view method without the
+        HTTP_REFERER header:
+            - That it returns the homepage view
+        """
+        self.factory = RequestFactory()
+        request = self.factory.get('/help')
+        referer = get_referer_view(request)
+        self.assertEqual(referer, "/")
+
+    def test_method_returns_current_page_with_referer(self):
+        """
+        Test that when you call the get_refer_view method with the
+        HTTP_REFERER header:
+            - That it returns the page on the HTTP_REFERER header
+        """
+        self.factory = RequestFactory()
+        request = self.factory.get('/help')
+        request.META['HTTP_REFERER'] = '/help'
+        referer = get_referer_view(request)
+        self.assertEqual(referer, "/help")
