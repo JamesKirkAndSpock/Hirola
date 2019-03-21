5541aba130a21c13dc029c546ad2c7594456050e
xcixor <pndungu54@gmail.com>
2019-03-21 23:07:58 +0300

    chore (display): change features display to list format

--- hirola/front/static/front/js/hot_deal_quantity.js
+++ hirola/front/static/front/js/hot_deal_quantity.js
@@ -1,5 +1,12 @@
 $('#quantity').change(function() {
     $.getJSON("/hot_deal_quantity_change", {qty: $('#quantity').val(), phone_model_item: $('#phone_item').val(), view: 'json'}, function(j) {
-    $('#price').html(j.currency + "  " + j.total_cost);
+        $total = comma(j.total_cost)
+        $('#price').html(j.currency + "  " + $total);
     });
 });
+
+function comma(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
