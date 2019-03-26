df795e4c9887db384899cfe41fd7458918b3aca2
xcixor <pndungu54@gmail.com>
2019-03-27 01:51:45 +0300

    chore(views): implement chore reviews
    - Fix linting errors

--- hirola/front/static/front/js/checkout.js
+++ hirola/front/static/front/js/checkout.js
@@ -1,18 +1,10 @@
-var $shipping = document.getElementById('shippingMethod');
-// console.log($shipping);
-// for (var i = 0; i < $shipping.length; i++){
-//     if ($shipping[i].checked == true){
-//         console.log($shipping[i]);
-//     }
-// }
 $(document).ready(function(){
     $('#shippingMethod').click(function(){
         var radioValue = $("input[name='group1']:checked").attr('id');
         if(radioValue === 'delivery'){
-            $('.payment').css('display', 'block')
+            $('.payment').css('display', 'block');
         }else {
-            $('.payment').css('display', 'none')
+            $('.payment').css('display', 'none');
         }
     });
-
 });
