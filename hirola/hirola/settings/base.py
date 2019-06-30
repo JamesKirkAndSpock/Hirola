92142f080573df32d35ca3aa31a02fbcb4f9d725
xcixor <pndungu54@gmail.com>
2019-06-30 17:37:06 +0300

    fix(erroneous dependency): refactor app to stop using django_inlinecss
    - Uninstall django_inlinecss package
    
    - Use inline css to style email

--- hirola/hirola/settings/base.py
+++ hirola/hirola/settings/base.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'django_inlinecss'
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
